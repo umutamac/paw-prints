59b5430f1b0f1fad5ff6d9e81a7199d953ac3cd4
Umut Alptekin <amacalptekin@gmail.com>
2020-12-01 18:56:00 +0300

    created a react page for upload page, and modifiying the back end to match it

--- client/src/pages/UploadPost.js
+++ client/src/pages/UploadPost.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from 'react-router-dom'
 
+import axios from "axios";
 
 function UploadPost() {
-    return (
-        <div id="UploadPostContainer">
-            
-        </div>
+    const [redirect, setRedirect] = useState(false)
 
+    const uploadform = (event) => {
+        event.preventDefault()
+
+        //package file info and send it back
+        var formData = new FormData();
+
+        axios.post('/api/pics/imgup', formData).then((response) => {
+            
+            console.log(response)
+            setRedirect(true)
+        })
+    }
+    return (
+        <div>
+            {redirect ? <Redirect push to='/home' /> : <div id="redirect"></div>}
+
+            <div id="UploadPostContainer">
+
+                <h1>Create a post</h1>
+                <p>You will need to upload a picture of your pet</p>
+
+                <input id="petName" type='text' name="petName" placeholder="Pet Name" encType="multipart/form-data" />
+                <input id="petImg" type='file' name="file" accept="image/*" encType="multipart/form-data" />
+                <input id="petType" type='text' name="petType" placeholder="Type of Animal" encType="multipart/form-data" />
+                <textarea id="petExplanation" type='text' name="petExplanation" placeholder="What does the pet look like, when was it last seen?" encType="multipart/form-data" />
+
+                <button id="uploadBtn" type='button' onClick={uploadform}>Upload</button>
+
+            </div>
+        </div>
     );
 }
 
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,49 +1,46 @@
 const router = require("express").Router();
 // const db = require("../../models");
-
-/////// bring in fs to delete file when finished
-const fs=require('fs')
+const fs = require('fs') // bring in fs to delete file when finished
 const postController = require("../../controllers/postController");
 require('dotenv').config();
 
 //import multer and create a folder "uploads" to hold on to temp files
-const multer  = require('multer')
+const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
 ///import cloudinary and configure to your bucket access
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({ // look at env for cloudinary config variables
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 router.route("/mypics")
   .get(postController.findAll)
   .post(postController.create);
 
-
-// router
-//   .route("/:id")
-//   .get(postController.findById)
-//   .put(postController.update)
-//   .delete(postController.remove);
-
-////////create reference to img url in mongodb
-router.route('/dbpic')
-.post(postController.create)
+router.route("/:id")
+  .get(postController.findById)
+  .delete(postController.remove);
+// .put(postController.update)
 
 //////////////// use multer upload method to organize file data to readable format
-router.post("/imgup", upload.single('file'),function(req,res, next){
-  console.log(req.file)
+router.post("/imgup", upload.single('file'), function (req, res, next) {
+
+ // the req.body has the text inputs, and req.file
+
+  console.log(req.body)
+
   ///////////use cloudinary uploader to send file to bucket  and upload response
   cloudinary.uploader.upload(req.file.path, { tags: 'express_sample' })
     .then(function (image) {
       console.log('** file uploaded to Cloudinary service');
       console.dir(image);
+
       ////save the file path to temp folder and delete file
-      console.log(req.file.path+"\n^^^^^^^^^^^^^^")
-      fs.unlink(req.file.path, err=>{if(err){console.log(err)}})
+      console.log(req.file.path + "\n^^^^^^^^^^^^^^")
+      fs.unlink(req.file.path, err => { if (err) { console.log(err) } })
       res.json(image.url)
     })
     .then(function () {
