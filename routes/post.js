d695dca78e41d5c3f3b96b2ad01c61f531536a12
Delmy Arguello <kroarguelloadel@gmail.com>
2020-12-02 13:37:23 -0600

    deleting 2 files in routes

--- routes/post.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
-import postRoutes from './routes/';
-
-const app = express(); 
-
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
