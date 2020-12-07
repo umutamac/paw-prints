e29bba41f49d99788cab5d81c13beeaef82f68e5
Umut Alptekin <amacalptekin@gmail.com>
2020-12-07 22:27:17 +0300

    added getmyposts to dashboaad with turnery operator

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import { logoutUser } from "../../actions/authActions";
 import "../layout/footer.css"
 
+import GetMyPosts from "../layout/GetMyPosts";
 import GetAllPosts from "../layout/GetAllPosts";
 
 class Dashboard extends Component {
@@ -19,38 +20,38 @@ class Dashboard extends Component {
     //console.log(user)
     return (
       <div className="background">
-          <div style={{ height: "75vh" }} className="container valign-wrapper">
-            <div className="row">
-              <div className="col s12 cell center-align">
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+          <div className="row">
+            <div className="col s12 cell center-align">
               <h1 id="title">PawPrints</h1>
-    
+
               <h3 className="nerko">Helping You Find Your Lost Companion</h3>
-              <hr/>
-                <h4 className="nerko">
-                  <b className="nerko">Hey there,</b> {user.name.split(" ")[0]}
-                  <p className="flow-text grey-text text-darken-1">
-                    
-                  </p>
-                  <button className="radius success button large">
-                    <Link to="/upload" style={{ color: "black"}}>Create a post</Link>
-                  </button>
-                </h4>
-                <button type="submit" onClick={this.onLogoutClick} class="radius success button large" id="signUpButton">
-                              Logout
+              <hr />
+              <h4 className="nerko">
+                <b className="nerko">Hey there,</b> {user.name.split(" ")[0]}
+                <p className="flow-text grey-text text-darken-1">
+
+                </p>
+                <button className="radius success button large">
+                  <Link to="/upload" style={{ color: "black" }}>Create a post</Link>
+                </button>
+              </h4>
+              <button type="submit" onClick={this.onLogoutClick} class="radius success button large" id="signUpButton">
+                Logout
                 </button>
 
-              </div>
-
             </div>
+
           </div>
-          <div className="hero-section noBackground">
-                <div className="col s12 center-align">
-                  <GetAllPosts /> 
-                  {/* 
-                    The home page for loggen in people will include this page.
-                    After creating a post, people will be redirected here
-                  */}
-                </div>
+        </div>
+        <div className="hero-section noBackground">
+          <div className="col s12 center-align">
+            {true
+              ? <GetAllPosts />
+              : <GetMyPosts />
+              // The home page for loggen in people will include one of these components
+            }
+          </div>
         </div>
       </div>
     );
