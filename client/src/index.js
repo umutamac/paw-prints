91b0082f932c00a16499b2f3cb9b780f49faac87
Umut Alptekin <amacalptekin@gmail.com>
2020-12-12 17:56:31 +0300

    redux dev tools installed

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//import { Provider } from "react-redux";
 import './index.css';
 import 'foundation-sites/dist/css/foundation.min.css';
 
