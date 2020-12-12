10da70dff9812ab9d67085bfccca1cfbcd5fdaf6
Umut Alptekin <amacalptekin@gmail.com>
2020-12-12 20:47:39 +0300

    npm install --save redux-devtools-extension done in store.js in client

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
+ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
 const middleware = [thunk];
@@ -8,9 +9,9 @@ const store = createStore(
   rootReducer,  
   //() => [],
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+    //,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
   )
 );
 
